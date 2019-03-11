78a6b033bd6b9991e5b053da87584e0eb725691f
Madi <45874448+QuantumMadi@users.noreply.github.com>
2019-03-11 20:59:45 +0600

    Fibbonachi, sum of two numbers

--- 20190304_Theory/Fibbonachi/Program.cs
+++ 20190304_Theory/Fibbonachi/Program.cs
@@ -11,39 +11,33 @@ namespace Fibbonachi
     {
         static void Main(string[] args)
         {
+            string result = string.Empty;
+            using (FileStream fs = File.Open("fibbonachi.txt", FileMode.OpenOrCreate))
+            {
+                if (fs.Length == 0)
+                {
+                    fs.Close();
+                    File.WriteAllText("fibbonachi.txt", string.Join("", "1 1 2 3 5"));
 
-           string result = string.Empty;
-
-           using (FileStream fs = File.OpenRead("fibbonachi.txt"))
+                }
+            }
+            using (FileStream fs = File.Open("fibbonachi.txt", FileMode.OpenOrCreate))
             {
                 byte[] buffer = new byte[fs.Length];
                 fs.Read(buffer, 0, buffer.Length);
 
                 result = Encoding.UTF8.GetString(buffer);
-                //Console.WriteLine(result + " result.Length = " + result.Length);
-            }
-                        
+
+            }                                 
             List<int> fibbonachiOld = result.Split(' ').Select(n => Convert.ToInt32(n)).ToList();
             int iterations = fibbonachiOld.Count;
             for (int i = 0; i < iterations; ++i)
-            {
-                
+            {                
                 fibbonachiOld.Add(fibbonachiOld[fibbonachiOld.Count-1]+fibbonachiOld[fibbonachiOld.Count-2]);
             }
-
             string newFibbonachi = string.Join(" ", fibbonachiOld.ToArray());
-            //using (var fs = File.OpenWrite("fibbonachi.txt"))
-            //{
-                File.WriteAllText("fibbonachi.txt", string.Join(" ", newFibbonachi));
-                //byte[] buffer = new byte[newFibbonachi.Length * sizeof(char)];
-                //System.Buffer.BlockCopy(newFibbonachi.ToCharArray(), 0, buffer, 0, buffer.Length);
-                //fs.Write(buffer, 0, buffer.Length);
-           // }
-           
-
+            File.WriteAllText("fibbonachi.txt", string.Join(" ", newFibbonachi));
             
-
-           // Console.ReadLine();
         }
     }
 }
--- 20190304_Theory/SumOFTwoNumbers/Program.cs
+++ 20190304_Theory/SumOFTwoNumbers/Program.cs
@@ -5,19 +5,38 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
+
 namespace SumOFTwoNumbers
 {
     class Program
     {
         static void Main(string[] args)
         {
-            using (var fs = File.OpenRead("file.txt"))
+            string result;
+            using (var fs = File.Open("input.txt", FileMode.OpenOrCreate))
+            {
+                                            
+                if (fs.Length == 0)
+                {
+                    Random firstRand = new Random();
+                    Random secondRand = new Random(10);
+                    int qw = firstRand.Next()%100;
+                    int rt = secondRand.Next() % 100;
+                    fs.Close();
+                    File.WriteAllText("input.txt", $"{firstRand.Next()%100} {secondRand.Next()%100}");
+                }
+            }
+
+            using (var fs = File.Open("input.txt", FileMode.OpenOrCreate))
             {
                 byte[] buffer = new byte[fs.Length];
                 fs.Read(buffer, 0, buffer.Length);
 
                 result = Encoding.UTF8.GetString(buffer);
             }
+
+            List<int> twoNumbers = result.Split(' ').Select(n => Convert.ToInt32(n)).ToList();
+            File.WriteAllText("output.txt", string.Join("", twoNumbers[0] + twoNumbers[1]));
         }
     }
 }
